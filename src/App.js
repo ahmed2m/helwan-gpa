1f53163ba58ebcd350742a337af6c94a1c8b0215
ahmeed2m <2m.fcih@fci.helwan.edu.eg>
2019-07-22 10:04:49 +0200

    progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Card,Icon,Segment,Dropdown,Checkbox,Header} from "semantic-ui-react";
+import {Label,Grid, Responsive, Card, Icon, Segment, Dropdown, Checkbox, Header, Button, Input} from "semantic-ui-react";
 
 class App extends React.Component{
     state={};
@@ -13,22 +13,48 @@ class App extends React.Component{
     };
     render() {
         return (
-            <div>
-                <Header as='h2' icon textAlign='center' style={{padding:'10px'}}>
+            <>
+            <div className='ui main'>
+                <Header as='h1' center textAlign='center'  style={{padding:'10px'}} dividing >
                     <Icon name='glide g' circular />
-                    <Header.Content>GPA Calculator</Header.Content>
+                    GPA Calculator
                 </Header>
-                <Subject/>
-                <Segment style={this.style}>
-                    <Segment as='a' href={'http://github.com/ahmeed2m'}  target="_blank">
-                        <Icon name='like'/> me on github
-                    </Segment>
-                </Segment>
+                <Grid columns={1}>
+                    <Grid.Column>
+                        <Responsive>
+                            <div className='subjects'>
+                                <PrevGPA/>
+                                <SubjectList/>
+                            </div>
+                            <div className='ui basic content center aligned segment'>
+                                <NewSubject/>
+                            </div>
+                            <Segment style={this.style}>
+                                <Segment as='a' href={'http://github.com/ahmeed2m'}  target="_blank">
+                                    <Icon name='like'/> me on github
+                                </Segment>
+                            </Segment>
+                        </Responsive>
+                    </Grid.Column>
+                </Grid>
             </div>
+            </>
         );
     }
 }
 
+class PrevGPA extends React.Component{
+    render() {
+        return(
+            <Card centered>
+                <Label style={{'margin-right':'0px'}}>Previous GPA</Label>
+                <Input type='number' min='1' onChange={this.handlePrevGPA}/>
+                <Label style={{'margin-right':'0px','margin-left':'0px'}}>Previous Completed Hours</Label>
+                <Input type='number' min='1' onChange={this.handlePrevHours}/>
+            </Card>
+        );
+    }
+}
 class SubjectList extends React.Component {
     render(){
         return(
@@ -86,6 +112,12 @@ class Subject extends React.Component{
                         onChange={this.handleGradeChange}
                     />
                     <Card.Content extra>
+                        <Button
+                            floated={'right'}
+                            icon={'trash'}
+                            color={'red'}
+                            basic
+                        />
                         Subject GPA : {this.state.subjectGPA}
                         <br/>
                         Hours : {(this.state.checked)?'2':'3'}
@@ -95,5 +127,14 @@ class Subject extends React.Component{
     }
 }
 
-
+class NewSubject extends React.Component{
+    render() {
+        return (
+            <Button
+                centered
+                icon='plus'
+            />
+        );
+    }
+}
 export default App;
