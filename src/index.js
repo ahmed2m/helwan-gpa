d2a458ee9f206b1992d722fe0130890ae3d9fe17
ahmed <2m.fcih@fci.helwan.edu.eg>
2019-10-06 13:57:13 +0200

    Moved to Hooks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App_function';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css'
 
